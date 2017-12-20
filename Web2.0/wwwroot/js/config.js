d0c2734404fa85e4f9f9235e015d178580289c3a
Chien Dang <chiendang.mr@gmail.com>
2017-12-20 18:02:34 +0700

    Add fontello

---
+++ Web2.0/wwwroot/js/config.js
@@ -0,0 +1,16 @@
+﻿require.config({
+    baseUrl: 'lib',
+    paths: {        
+        'domReady': 'domReady/domReady',
+        'jquery': "jquery/dist/jquery",        
+        'bootstrap': "bootstrap/dist/js/bootstrap",        
+        'jquery.validate': 'jquery-validation/dist/jquery.validate',
+        'additional-methods': 'jquery-validation/dist/additional-methods',
+        'jquery.validate.unobtrusive': 'jquery-validation-unobtrusive/jquery.validate.unobtrusive'
+    },    
+    shim: {
+        'toolkit': ["jquery"],
+        'jquery.validate': ['jquery'],
+        'jquery.validate.unobtrusive': ['jquery.validate']        
+    }
+});
